6854f7c679f2572cbbcc02a4dfbfce323743385e
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-15 17:23:29 +0800

    [Mars Rover][John Carlos]test: implement should return 0,0 W when given 0,0 N with move L

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -8,12 +8,24 @@ class DemoTest {
     @Test
     void should_return_x_0_y_1_dir_N_when_given_x_0_y_0_dir_N_move_M() {
         //given
-        marsRover marsRover = new marsRover(0,0,'N');
+        marsRover marsRover = new marsRover(0, 0, 'N');
         //when
         marsRover.executeMovement("M");
         //then
         assertEquals(0, marsRover.coordinateX());
         assertEquals(1, marsRover.coordinateY());
         assertEquals('N', marsRover.getDirection());
+    }
+
+    @Test
+    void should_return_x_0_y_0_dir_W_when_given_x_0_y_0_dir_N_move_L() {
+        //given
+        marsRover marsRover = new marsRover(0, 0, 'N');
+        //when
+        marsRover.executeMovement("L");
+        //then
+        assertEquals(0, marsRover.coordinateX());
+        assertEquals(0, marsRover.coordinateY());
+        assertEquals('W', marsRover.getDirection());
     }
 }
