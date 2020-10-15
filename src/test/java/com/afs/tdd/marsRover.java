13840dc62f523917ad8c770a786530a00d6094d2
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-15 17:21:18 +0800

    [Mars Rover][John Carlos]feature: added methods to accomodate first test case

---
+++ src/test/java/com/afs/tdd/marsRover.java
@@ -0,0 +1,44 @@
+package com.afs.tdd;
+
+import java.util.Arrays;
+
+public class marsRover {
+
+    private int coordX;
+    private int coordY;
+    private char direction;
+
+    public marsRover(int coordX, int coordY, char direction) {
+        this.coordX = coordX;
+        this.coordY = coordY;
+        this.direction = direction;
+    }
+
+    public void executeMovement(String movement) {
+        Arrays.asList(movement.split("")).forEach(moves -> this.execMove(moves));
+    }
+
+    private void execMove(String moves) {
+        if (moves.equals("M")) {
+            moveRover();
+        }
+    }
+
+    private void moveRover() {
+        if (direction == 'N') {
+            coordY += 1;
+        }
+    }
+
+    public int coordinateX() {
+        return coordX;
+    }
+
+    public int coordinateY() {
+        return coordY;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+}
