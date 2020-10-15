9102ea367a67cee2aad96755b575e530f4a14098
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-15 17:18:12 +0800

    [Mars Rover][John Carlos]test: implement should return 0,1 N when given 0,0 N with move M

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,18 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
-
+    void should_return_x_0_y_1_dir_N_when_given_x_0_y_0_dir_N_move_M() {
+        //given
+        marsRover marsRover = new marsRover(0,0,'N');
+        //when
+        marsRover.executeMovement("M");
+        //then
+        assertEquals(0, marsRover.coordinateX());
+        assertEquals(1, marsRover.coordinateY());
+        assertEquals('N', marsRover.getDirection());
     }
 }
