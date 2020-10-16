955db1e6ccd73026968609e4a4f77a45e76da05e
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-16 10:48:56 +0800

    [Mars Rover][John Carlos]refactor: change to validateCommand in multiple commands test

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -162,11 +162,11 @@ class DemoTest {
     }
 
     @Test
-    void should_return_x_negative_1_y_1_dir_N_when_given_x_0_y_0_dir_N_move_MLMR() {
+    void should_return_x_negative_1_y_1_dir_N_when_given_x_0_y_0_dir_N_move_MLMR() throws CommandNotDefinedException {
         //given
         marsRover marsRover = new marsRover(0, 0, 'N');
         //when
-        marsRover.execCommandsMany("MLMR");
+        marsRover.validateCommand("MLMR");
         //then
         assertEquals(-1, marsRover.coordinateX());
         assertEquals(1, marsRover.coordinateY());
