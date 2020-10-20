82d7b00bb93d47cf2f70efff85fd2b825f0a9685
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-21 00:57:33 +0800

    [Mars Rover][John Carlos]refactor: added default and constant UNEXPECTED VALUE

--- src/main/java/com/afs/tdd/marsRover.java
+++ src/main/java/com/afs/tdd/marsRover.java
@@ -1,16 +1,12 @@
 package com.afs.tdd;
 
-import java.util.Arrays;
-import java.util.List;
-
-public class marsRover {
-    private static final String MOVE_COMMAND = "M";
-    private static final String TURN_LEFT_COMMAND = "L";
-    private static final String TURN_RIGHT_COMMAND = "R";
+public class marsRover implements CommandCenter{
     private static final char NORTH = 'N';
     private static final char SOUTH = 'S';
     private static final char EAST = 'E';
     private static final char WEST = 'W';
+
+    private static final String UNEXPECTED_VALUE = "Unexpected value: ";
     private int coordinateX;
     private int coordinateY;
     private char direction;
@@ -21,32 +17,8 @@ public class marsRover {
         this.direction = direction;
     }
 
-    public void validateCommand(String validCommand) throws CommandNotDefinedException {
-        String[] inputCommands = validCommand.split("");
-        List<String> validCommands = Arrays.asList(MOVE_COMMAND, TURN_LEFT_COMMAND, TURN_RIGHT_COMMAND);
-
-        for (String inputCommand : inputCommands) {
-            if (!(validCommands.contains(inputCommand))) {
-                throw new CommandNotDefinedException("Command not valid : " + inputCommand);
-            } else execCommand(inputCommand);
-        }
-    }
-
-    public void execCommand(String commands) {
-        switch (commands) {
-            case MOVE_COMMAND:
-                moveRover();
-                break;
-            case TURN_LEFT_COMMAND:
-                turnLeft();
-                break;
-            case TURN_RIGHT_COMMAND:
-                turnRight();
-                break;
-        }
-    }
-
-    private void turnRight() {
+    @Override
+    public void turnRight() {
         switch (direction) {
             case NORTH:
                 direction = EAST;
@@ -60,27 +32,13 @@ public class marsRover {
             case WEST:
                 direction = NORTH;
                 break;
+            default:
+                throw new IllegalStateException(UNEXPECTED_VALUE + direction);
         }
     }
 
-    private void turnLeft() {
-        switch (direction) {
-            case NORTH:
-                direction = WEST;
-                break;
-            case SOUTH:
-                direction = EAST;
-                break;
-            case EAST:
-                direction = NORTH;
-                break;
-            case WEST:
-                direction = SOUTH;
-                break;
-        }
-    }
-
-    private void moveRover() {
+    @Override
+    public void moveRover() {
         switch (direction) {
             case NORTH:
                 coordinateY++;
@@ -94,6 +52,28 @@ public class marsRover {
             case WEST:
                 coordinateX--;
                 break;
+            default:
+                throw new IllegalStateException(UNEXPECTED_VALUE + direction);
+        }
+    }
+
+    @Override
+    public void turnLeft() {
+        switch (direction) {
+            case NORTH:
+                direction = WEST;
+                break;
+            case SOUTH:
+                direction = EAST;
+                break;
+            case EAST:
+                direction = NORTH;
+                break;
+            case WEST:
+                direction = SOUTH;
+                break;
+            default:
+                throw new IllegalStateException(UNEXPECTED_VALUE + direction);
         }
     }
 
